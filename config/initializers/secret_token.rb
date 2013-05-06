9456be9abb1d8d60423dddd0d6b48777ff7ad9f4
Joey Parshley <jparshley@constantcontact.com>
2013-05-06 19:02:55 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LittleLeague::Application.config.secret_token = '8fd66a3fc9a1bc6dc048ef030bb9f2383757b83a885e553325553e823c91be212dd25168e507db2bed004c9aed480c2be666ddec0234bd6384bd12a50e17b8ed'
